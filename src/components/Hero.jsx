c3a7e8ecea4c89a0dc11b1a34ec882137a82ff16
Boluwatife <omotayodaniel10@gmail.com>
2023-02-07 21:31:35 +0100

    feat: nav toggle done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,25 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { FaBars, FaTimes } from "react-icons/fa";
+
 import Logo from "../assets/logo.png";
 import Map from "../assets/map.png";
 import Search from "../assets/icon-search-blue.png";
 
 const Hero = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+  const screenWidth = window.innerWidth;
+
+  const toggleNav = () => {
+    setToggleMenu(!toggleMenu);
+  };
+
   return (
     <section
       id="Home"
       className="bg-[url('/src/assets/hero-bg.png')] bg-heroBackgroundColor bg-center bg-cover bg-no-repeat h-screen"
     >
       <div className="lg:px-[8%] px-[4%] pt-[2%] ">
-        <div className="mb-2 border-b border-b-white lg:flex lg:items-end lg:justify-between w-full">
+        <header className="relative lg:flex lg:items-end lg:justify-between w-full mb-2">
           <img src={Logo} alt="" />
-          <nav className="relative lg:flex display-none lg:justify-around lg:items-end w-[100%] lg:ml-[15%] lg:mr-[20%]">
-            <a href="#">Home</a>
-            <a href="#">Landlord</a>
-            <a href="#">Tenants</a>
-            <a href="#">Contact Us</a>
-          </nav>
-        </div>
-        <div className="flex flex-col mt-[5rem] lg:mt-0 text-center lg:text-left lg:flex-row items-center justify-between ">
+          {(toggleMenu || screenWidth > 1024) && (
+            <nav className="flex lg:flex-row flex-col lg:justify-around justify-center lg:items-end items-center w-full h-full bg-light_black lg:bg-opacity-0 lg:relative fixed lg:ml-[15%] lg:mr-[20%] z-10 top-0 left-0">
+              <a href="#">Home</a>
+              <a href="#">Landlord</a>
+              <a href="#">Tenants</a>
+              <a href="#">Contact Us</a>
+              <button onClick={toggleNav} className="text-white">
+                <FaTimes className="text-sm lg:hidden absolute top-[3%] right-[3%]" />
+              </button>
+            </nav>
+          )}
+          {!toggleMenu && (
+            <button onClick={toggleNav} className="text-white">
+              <FaBars className="text-sm lg:hidden absolute top-[15%] right-[5%]" />
+            </button>
+          )}
+        </header>
+        <div className="flex flex-col mt-[3rem] lg:mt-0 text-center lg:text-left lg:flex-row items-center justify-between ">
           <h1 className="text-white font-bold lg:w-[45%]">
             The Most Affordable Place To Stay In The San Franciso Bay Area
           </h1>
